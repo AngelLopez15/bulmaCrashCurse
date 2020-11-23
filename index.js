af9614fb75d0574d3b78f4b56a8ba52813ab3eed
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-11-22 23:34:42 -0600

    Navbar

---
+++ index.js
@@ -0,0 +1,7 @@
+// Para activer el menu en moviles
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', ()=>{
+  navbarMenu.classList.toggle('is-active');
+})
